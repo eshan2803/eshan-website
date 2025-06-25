2cffb15bd3079878d37c58aec7b02ddc47d2fc07
eshan2803 <104783677+eshan2803@users.noreply.github.com>
2025-06-25 11:03:33 -0700

    app.py

--- app.py
+++ app.py
@@ -2331,7 +2331,7 @@ def run_lca_model(inputs):
             f"• Production Cost in {start}: ${hydrogen_production_price:.2f}/kg*\n"
             f"• Total Transport Cost: ${chem_cost:.2f}/kg\n"
             f"• Transport cost is {ratio_cost:.1f} times the production cost.\n\n"
-            f"*Hydrogen production cost is an estimate. Take with a pinch of salt.\n"
+            f"*These costs are estimates. Take with a pinch of salt.\n"
             f" Working to improve this."
         )
 
